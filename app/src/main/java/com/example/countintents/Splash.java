63e1ec192ca7c013eaeabd32d7ab3285250ec01a
DiegoDuenez <19170154@uttcampus.edu.mx>
2020-10-19 19:09:48 -0500

    Subida de archivos de la practica

---
+++ app/src/main/java/com/example/countintents/Splash.java
@@ -0,0 +1,44 @@
+package com.example.countintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        txt1 = findViewById(R.id.textseg);
+
+        new CountDownTimer(5000, 1000) {
+
+
+            @Override
+            public void onTick(long l) {
+                txt1.setText(String.valueOf(l / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+
+            }
+
+
+        }.start();
+
+
+    }
+
+}
